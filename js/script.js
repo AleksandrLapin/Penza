5736435fc30033e98f202fe5acf05fef0f639bd0
AleksandrLapin <65232865+AleksandrLapin@users.noreply.github.com>
2020-09-29 23:02:45 +0300

    10

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-// $(document).ready(function(){
-// 	$('.button_burger').click(function(event) {
-// 		$('.button_burger,.header_menu').toggleClass('active');
-// 	});
-// });
-
-$('.button_burger').click(function(){
-        $('.button_burger, .header_menu').toggleClass('active');
-	$('body').toggleClass('lock');
-});
-
-$('.header_menu').click(function(){
-       $('.button_burger, .header_menu').removeClass('active');
-       $('body').removeClass('lock');
+$(document).ready(function(){
+	$('.button_burger').click(function(event) {
+		$('.button_burger,.header_menu,.reg_box').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$('.button_burger').click(function(event) {
-		$('.button_burger,.header_menu').toggleClass('active');
+		$('.button_burger,.reg_box').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
+// .header_menu,
